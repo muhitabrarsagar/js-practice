86d9b376d2f75a3d9f51dea363057f175a4bcd1b
Muhit Abrar Sagar <muhitabrarsagar@Muhits-MacBook-Pro.local>
2023-07-10 23:35:39 +0600

    initial commit

---
+++ chapter_two/javas.js
@@ -0,0 +1,107 @@
+//5 probo console.log how to work
+
+
+// console.log('my name is sagar');
+// console.log('10'+1);
+// console.log('my number is: ' + 01839341290);
+
+//6 porbo var
+
+// var name ="Muhit abrar sagar"
+// var age="23"
+// console.log('My namne is '+ name);
+// console.log('I am '+ age +' years old');
+
+//7 probo Reserved Words 
+
+// var myNameIs='sagar';=this name is folwed by camel case
+// console.log(myNameIs);
+
+//8 porbo Data type
+
+//2 type of js are available
+// 1)primitive = number, string, boolen, undifined, null
+// 2)object = array, object, function
+
+
+//9 porbo Data type=primitive = number
+
+// var n = Number(54.6)
+// console.log(Number.parseInt(n))
+// console.log(Number.parseFloat(n))
+
+//10 string start 
+//  var theStringStart = String(100);
+
+//  console.log(theStringStart )
+// var x = 12
+// console.log(x.toString());
+
+//14 porbo hex and oct number
+
+// var hex = 0xff;
+// console.log(hex)
+
+// var oct = 0756
+// console.log(oct);
+
+//15 porbo operator
+
+// var a = 10;
+// var b = 20;
+
+// a += b;
+// console.log(a);
+
+// a -= b;
+// console.log(a);
+
+// a *= b;
+// console.log(a);
+
+// a /= b;
+// console.log(a);
+
+//porbo 17 math
+
+// var n = 5.69
+
+// console.log(Math.abs(n))
+// console.log(Math.floor(n))
+// console.log(Math.round(n))
+// console.log(Math.ceil(n))
+// console.log(Math.pow(10,10))
+// console.log(Math.sqrt(n))
+// console.log(Math.round(Math.random() * 50 + 1 ))
+
+//porbo 18 Date functions
+
+// var date = new Date();
+
+// console.log(date.toDateString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toTimeString())
+// console.log(date.getFullYear())
+// console.log(date.getMonth() +1)
+// console.log(date.getDate())
+
+// var n = 20;
+
+// if(n<=20)
+// {
+//     console.log('This number is ' +n+' is bigger then')
+// }else{
+//     console.log('This number is '+n+' is smaller then')
+// }
+
+
+// var age = 50
+// if (age>=18){
+    
+//     console.log('entry');
+// }else{
+//     console.log('no entry');
+// }
+
+
+
